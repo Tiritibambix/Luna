01c53c915b2061afa15d3ac680b2742aa6df667f
Opisek <darowski.kacper@gmail.com>
2026-05-05 02:15:32 +0200

    prepare frontend for backup encryption

--- frontend/src/components/modals/settingModalTabs/BackupsSettingsTab.svelte
+++ frontend/src/components/modals/settingModalTabs/BackupsSettingsTab.svelte
@@ -6,6 +6,12 @@
   import { downloadFileToClient, fetchFile, fetchResponse } from "$lib/client/net";
   import { queueNotification } from "$lib/client/notifications";
   import FileUpload from "../../forms/FileUpload.svelte";
+  import Modal from "../Modal.svelte";
+  import { NoOp } from "$lib/client/placeholders";
+  import IconButton from "../../interactive/IconButton.svelte";
+  import { Check, X } from "lucide-svelte";
+  import ToggleInput from "../../forms/ToggleInput.svelte";
+  import TextInput from "../../forms/TextInput.svelte";
 
   interface Props {
     settings: Settings;
@@ -17,18 +23,46 @@
     showConfirmation,
   }: Props = $props();
 
+  let showEncryptionModal: () => Promise<string> = $state(Promise.reject);
+  let success: (result: string) => void = $state(NoOp);
+  let failure: (reason?: string | Error) => void = $state(NoOp);
+
+  let enableEncryption = $state(true);
+  let creatingBackup = $state(false);
+  let backupPassword = $state("");
+
   let backupFile: FileList | null = $state(null);
 
   async function createBackup() {
-    await fetchFile("/api/backups/create", { method: "POST" }).then(downloadFileToClient).catch(e => {
+    creatingBackup = true;
+    backupPassword = "";
+    const password = await showEncryptionModal().catch(() => null);
+    if (!password) {
+      backupPassword = "";
+      return;
+    }
+
+    const formData = new FormData();
+    if (password != "") formData.append("backup_password", password);
+
+    await fetchFile("/api/backups/create", { method: "POST", body: formData }).then(downloadFileToClient).catch(e => {
       queueNotification(ColorKeys.Danger, e);
+    }).finally(() => {
+      backupPassword = "";
     })
   }
 
   async function restoreBackup() {
-    console.log(backupFile);
     if (backupFile === null) {
       queueNotification(ColorKeys.Danger, t("settings.backups.error.file"));
+      return;
+    }
+
+    creatingBackup = false;
+    backupPassword = "";
+    const password = await showEncryptionModal().catch(() => null);
+    if (!password) {
+      backupPassword = "";
       return;
     }
 
@@ -37,11 +71,14 @@
 
     const formData = new FormData();
     formData.append("backup_file", backupFile[0]);
+    if (password != "") formData.append("backup_password", password);
 
     return fetchResponse("/api/backups/restore", { method: "POST", body: formData }).then(() =>
       showConfirmation(t("settings.backups.restart"), "", true)
     ).catch(err => {
       queueNotification(ColorKeys.Danger, t("settings.backups.error.restore", { values: { msg: err.message } }));
+    }).finally(() => {
+      backupPassword = "";
     })
   }
 </script>
@@ -59,3 +96,26 @@
     {t("settings.backups.restore")}
   </Button>
 {/if}
+
+<Modal
+  title={t("settings.backups.encryption.title")}
+  bind:showModal={showEncryptionModal}
+  bind:success
+  bind:failure
+>
+  {creatingBackup ? t("settings.backups.encryption.desc.create") : t("settings.backups.encryption.desc.restore")}
+  <ToggleInput
+    name="backup_encrypted"
+    description={creatingBackup ? t("settings.backups.encryption.enable.create") : t("settings.backups.encryption.enable.restore")}
+    bind:value={enableEncryption}
+  />
+  {#if enableEncryption}
+    <TextInput bind:value={backupPassword} name="backup_password" placeholder={t("settings.backups.encryption.password")} password={true} />
+  {/if}
+  {#snippet buttons()}
+    <IconButton enabled={!enableEncryption || backupPassword.length > 0} onClick={() => success(enableEncryption ? backupPassword : "")} color={ColorKeys.Success} type="submit" alt={t("button.confirm")} canRenderAsButton={true}>
+      <Check/>
+    </IconButton>
+    <IconButton onClick={failure} color={ColorKeys.Danger} alt={t("button.cancel")} canRenderAsButton={true}><X/></IconButton>
+  {/snippet}
+</Modal>
